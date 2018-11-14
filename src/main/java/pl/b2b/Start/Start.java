4a69a3c184781a6ac7696d3e4e7fc6b07ff0ff7a
heso6 <romaniuk99@gmail.com>
2018-11-14 14:00:45 +0100

    Initial commit!

--- src/main/java/pl/b2b/Start/Start.java
+++ src/main/java/pl/b2b/Start/Start.java
@@ -2,18 +2,54 @@ package pl.b2b.Start;
 
 public class Start {
 
-    //getName(String name)
+    private String name = "pablito";
+
+    public String getName(String name) {
+        return name;
+    }
 
 
-    //getNameAndSurname(String name, String surname)
+    public void setName(String name) {
+        this.name = name;
 
-    //plus(int firs, int second)
+    }
 
-    //minus(int firs, int second)
 
-    //multiplication(int firs, int second)
+    public String getNameAndSurname(String name, String surname) {
+        return name + surname;
+    }
 
-    //division(int firs, int second)
+    public int plus(int first, int second) {
+        int sum = 0;
+        sum += first;
+        sum += second;
+        return sum;
+    }
 
-    //ageNameSurname(int age, String nameSurname)
+   public int minus(int first, int second){
+        int minus = 0;
+        minus -= first;
+        second -= second;
+        return minus;
+   }
+
+   public int multiplication(int first, int second){
+        int multi = 0;
+        multi /= first;
+        multi /= second;
+        return multi;
+   }
+
+    public int division(int first, int second){
+        int divi = 0;
+        divi /= first;
+        divi /= second;
+        return divi;
+    }
+
+    public int ageNameSurname(int age, String nameSurname){
+         age = 20;
+        nameSurname = " pablito Kowalski";
+        return Integer.parseInt(age + nameSurname);
+    }
 }
