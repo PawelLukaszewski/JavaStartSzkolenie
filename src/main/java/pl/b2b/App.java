3ac613feedeb068e9760f27fde2e1c57b9b6956f
heso6 <romaniuk99@gmail.com>
2018-11-14 13:38:10 +0100

    Initial commit!

--- src/main/java/pl/b2b/App.java
+++ src/main/java/pl/b2b/App.java
@@ -1,41 +1,43 @@
 package pl.b2b;
 
-public class App 
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // System.out.println( "Hello World!" );
 
 
         // Wypisz swoje imię
-        System.out.println(" ");
+        System.out.println("Pablito");
 
 
         //wypisz swoje imie i nazwisko
-        String name;
-        String surname;
-
+        String name = "Pablito";
+        String surname = "Kowalski";
+        System.out.println(name + " " + surname);
 
         //Suma 4 i 9
-
+        System.out.println(4 + 9);
 
 
         //Wypisz wynik mnożenia, dzielenia i odejmowania liczb 3455 i 789
-        int first;
-        int second;
+        int first = 3455;
+        int second = 789;
+
+        int mnozenie = first * second;
+        System.out.println("wynik mnozenia to " + mnozenie);
+        int dzielenie = first / second;
+        System.out.println("wynik dzielenie to " + dzielenie);
+        int odejmowanie = first - second;
+        System.out.println("wynik odejmowanie to " + odejmowanie);
 
 
         //wypisz swoj wiek, imie i nazwisko
 
-        int age;
-        String nameAndSurname;
-
-
-
+        int age = 27;
+        String nameAndSurname = "pablito Kowalski";
+        System.out.println(age + ", " + nameAndSurname);
 
 
     }
-
-
 
 
 }
